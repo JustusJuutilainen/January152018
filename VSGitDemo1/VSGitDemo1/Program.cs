2eeeccd66588be4fa88814fab556902e4cdde05d
Student <Student@VMMB300-VHD-1>
2018-01-15 10:54:04 +0200

    Added VS project

---
+++ VSGitDemo1/VSGitDemo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGitDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+            Console.WriteLine("Is it me you looking for");
+            
+        }
+    }
+}
